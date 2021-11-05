ec8b2c8cb55503b1eea83ce8298b0d0b16e14d4c
Bhimsen04 <bhimsen1304@gmail.com>
2021-11-05 22:56:18 +0530

    String.join implemented

--- src/com/bhimsen/programs/string/AddNames.java
+++ src/com/bhimsen/programs/string/AddNames.java
@@ -2,6 +2,10 @@ package com.bhimsen.programs.string;
 
 public class AddNames {
     public static void main(String[] args) {
+        System.out.println(addNames("bhimsen" , "Garg"));
+    }
 
+    private static String addNames(String firstname, String lastname) {
+        return  String.join(" " , firstname , lastname);
     }
 }
