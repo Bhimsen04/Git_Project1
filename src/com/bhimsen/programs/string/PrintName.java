c357278b46889632940a4d3cca2c22d4720150bc
Bhimsen <38164379+Bhimsen04@users.noreply.github.com>
2021-11-29 13:52:26 +0530

    garg from git added in Printname

--- src/com/bhimsen/programs/string/PrintName.java
+++ src/com/bhimsen/programs/string/PrintName.java
@@ -3,5 +3,6 @@ package com.bhimsen.programs.string;
 public class PrintName {
     public static void main(String[] args) {
         System.out.println("print");
+        System.out.println("garg from Git");
     }
 }
